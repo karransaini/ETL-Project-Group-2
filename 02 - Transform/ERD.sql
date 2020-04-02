a8068cadb85935115143814b082ca8b336d20e55
karransaini <55826537+karransaini@users.noreply.github.com>
2020-04-01 20:59:14 -0700

    Add files via upload

---
+++ 02 - Transform/ERD.sql
@@ -0,0 +1,20 @@
+﻿--YouTube ERD
+
+youtube_stats
+-
+index INT PK
+video_id VARCHAR 
+trending_date VARCHAR
+title VARCHAR
+channel_title VARCHAR
+category_id INT FK >- categories.category_id
+publish_time VARCHAR
+views INT
+likes INT
+dislikes INT
+comment_count INT
+
+categories
+-
+category_id INT PK
+category VARCHAR
